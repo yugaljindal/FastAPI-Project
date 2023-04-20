0f1559c71519f42cf12514a93ca69508a8e70f1e
yugal <yugal.jindal@webners.com>
2023-04-20 12:40:50 +0530

    2023-04-20 YUG Added query parameters and pydantic to the fast api

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':{'name':'yugal'}}
+@app.get('/blog')
+def index(limit=10,published:bool=True,sort:Optional[str] = None):
+    # only get 10 published blog
+    if published:
+        return {'data':limit + ' blog from the db'}
+    else:
+        return {'data':limit + 'unpublished blog from the db'}
 
-@app.get('/about')
-def about():
-    return {'data':{'about page'}}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blog'}
+
+@app.get('/blog/{id}')
+def show(id:int):
+    return {'data':id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data':{'1','2'}}
